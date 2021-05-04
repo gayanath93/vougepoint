944bcf20044e613841e5ee3d9bf235bd015e052e
gayanath93 <gayanath93@gmail.com>
2021-05-05 00:32:32 +0900

    db path

--- config/connect.php
+++ config/connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$connection = mysqli_connect('localhost','root','1234','bimak');
+	$connection = mysqli_connect('us-cdbr-east-03.cleardb.com','bae7e4eb703d63','03a00eff','heroku_c59b078216b8358');
 	if(!$connection){
 		echo "Error: Unable to connect to MySQL." . PHP_EOL;
 		echo "Debugging errno: " . mysql_connect_errno() . PHP_EOL;
